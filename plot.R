f94e9174a4af95e06733f310ab6dcc5ef41e4d79
idaios <pavlidisp@gmail.com>
2023-01-12 21:23:36 +0200

    some plots

--- plot.R
+++ plot.R
@@ -1,7 +1,10 @@
 a <- read.table("homosapiens_feliscatus_ortho.txt", header=F)
-a[1,]
-plot(a[,3], a[,4])
+
 lmmod = lm(a[,4]~a[,3])
+pdf("correlationPlot.pdf")
+plot(a[,3], a[,4], xlab="Neighborhood similarity", ylab="Alignment percentage identity")
+abline(a=lmmod$coefficients[1], b=lmmod$coefficients[2], col='red', lwd=2)
+dev.off()
+
 summary(lmmod)
 lmmod$coefficients
-abline(a=lmmod$coefficients[1], b=lmmod$coefficients[2], col='red', lwd=2)
